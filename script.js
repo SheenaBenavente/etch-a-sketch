333376cc54753e14ea355b85c46efe19379a21cf
sheena benavente <13785060+SheenaBenavente@users.noreply.github>
2023-01-04 13:42:13 -0500

    added function to get size, loop for number of divs, and mouseover function for interaction.

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 document.addEventListener("DOMContentLoaded", function(){
     createContainer(32);
-    console.log("second")
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createContainer(size);
+    })
+    console.log("run all last")
 })
 
 function createContainer(size){
-    console.log("third");
+    console.log("createcontiner first");
     let container = document.querySelector(".container");
 
-    container.style.gridTemplateColumns = `repeat (${size}, 1fr)`;
-    container.style.gridTemplateRows = `repeat (${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 
-    let numDivs = size * size
+    let numDivs = size * size;
     
     for(let i = 0; i < numDivs; i++){
         let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
+        div.addEventListener("mouseover", function(){
+            div.style.backgroundColor = "black"
+        }) 
         container.insertAdjacentElement("beforeend", div);
     }
-    console.log("fourth")
+    console.log("createcontiner last");
 
+}
+
+function getSize(){
+    console.log("get size first");
+    let input = prompt("what is the size of your board?");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerHTML = "please provide a number";
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "provide a number between 1 and 100";
+    }
+    else {
+        message.innerHTML = "Start the Game!"
+        return input;
+    }
+    console.log("getsize last");
 }
